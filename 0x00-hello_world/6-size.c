a87d9fce89c2b1a25d7d778ee671317c8da05c83
Kamchad ALIDOU <kamchadalidou@gmail.com>
2022-06-30 14:49:40 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (success) 
+ */
+int main(void)
+{
+	int i;
+	long int I;
+	long long int II;
+	float d;
+	char c;
+
+	print("Size of char: %lu
+			byte(s)\n", size of(c));
+	printf("size of an int: %lu byte(s)\n" , size of (i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(I));
+	printf(""Size of a long long int: %lu byte(s)\n", sizeof(II));
+	print("Size of a float: %lu byte(s)\n", sizeof(d));
+	return (0);
+} 
