ef3f9e73c6e09bc56df6d5bd4fed29f5e4641966
Kamchad ALIDOU <kamchadalidou@gmail.com>
2022-07-04 08:07:22 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - entry point
+*
+* Rerurn: always returns 0
+*/
+int main(void)
+{
+int a = 0;
+for (a = 0; a <= 10; a++)
+putchar("0" - "10");
+putchar('\n');
+return (0);
+}
