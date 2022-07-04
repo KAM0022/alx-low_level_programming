17034a9cd214dc1ce348387c05b4ee7453a4e10a
Kamchad ALIDOU <kamchadalidou@gmail.com>
2022-07-04 09:10:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+* main - prints the lowcase alphabet in reverse
+*
+* Return - always returns 0
+*/
+int main(void)
+{
+char a;
+for (a = 'z'; a >= 'a'; a--)
+putchar(a);
+putchar('\n');
+return (0);
+}
