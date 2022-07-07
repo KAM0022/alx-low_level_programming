c2d49f84e636252b9c9fb85e02fc834c9e5165e7
Kamchad ALIDOU <kamchadalidou@gmail.com>
2022-07-07 12:21:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,16 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * main - check the code.
 *
 * Return: Always 0.
-*/
+*/ 
 int main(void)
 
 {
 
-	    int alphabet_x10();
+	    print_alphabet_x10();
 
 	        return (0);
 
